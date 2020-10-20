ff25f5cc1f947af4a7be79117ea5046a89760d57
pix666 <pix666@ya.ru>
2020-10-20 15:04:44 +0400

    compat django < 2.0

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+import django
 from .views import IndexView
+
+if django.VERSION >= (2, 0):
+    from django.urls import re_path
+else:
+    from django.conf.urls import url as re_path
 
 
 app_name = 'app'
 urlpatterns = [
-    path('', IndexView.as_view()),
+    re_path(r'', IndexView.as_view()),
 ]
--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path, include
+import django
 from django.contrib import admin
+
+if django.VERSION >= (2, 0):
+    from django.urls import re_path, include
+else:
+    from django.conf.urls import url as re_path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('app.urls')),
 ]
