c656286b79a3cdca8e9cbaa8321f04d4a7e87517
pix666 <pix666@ya.ru>
2020-10-21 12:41:05 +0400

    refactor js

--- awesomplete/static/awesomplete/js/widget.js
+++ awesomplete/static/awesomplete/js/widget.js
@@ -2,24 +2,10 @@
 
     function initWidgets($root, formsetName) {
         $root.find('.admin-awesomplete').each(function() {
-            var $input = $(this);
+            var input = this;
+            var $input = $(input);
             if ($input.closest('.empty-form').length) {
                 return
-            }
-
-            // Replace "__prefix__"
-            if (formsetName) {
-                var id_regex = new RegExp("(" + formsetName + "-(\\d+|__prefix__))");
-                var name_match = id_regex.exec($input.prop('name'));
-                var formsetIndex = name_match && name_match[2];
-                if (formsetIndex) {
-                    var replacement = formsetName + "-" + formsetIndex;
-                    if ($input.get(0).hasAttribute("list")) {
-                        $input.attr("list", $input.attr("list").replace(id_regex, replacement));
-                    } else if ($input.get(0).hasAttribute("data-list")) {
-                        $input.attr("data-list", $input.attr("data-list").replace(id_regex, replacement));
-                    }
-                }
             }
 
             // Django's "overflow:hidden" fix
@@ -28,23 +14,42 @@
                 $formRow.css('overflow', 'visible');
             }
 
-            var object = new Awesomplete(this, {
+            // Replace "__prefix__" in "list" and "data-list" attributes
+            if (formsetName) {
+                replaceFormsetPrefix(input, formsetName);
+            }
+
+            var instance = new Awesomplete(this, {
                 sort: function() {}
             });
 
             // when minChars is set to zero, show popup on focus.
-            if (object.minChars === 0) {
-                object.input.addEventListener('focus', function() {
+            if (instance.minChars === 0) {
+                instance.input.addEventListener('focus', function() {
                     var input = this;
                     if (input.value.length === 0) {
-                        object.evaluate()
+                        instance.evaluate()
                     }
                 });
             }
 
             // fix horizontal position (because of "float:left" on label)
-            object.ul.style.marginLeft = object.input.offsetLeft + 'px';
+            instance.ul.style.marginLeft = instance.input.offsetLeft + 'px';
         });
+    }
+
+    function replaceFormsetPrefix(input, formsetName) {
+        var id_regex = new RegExp("(" + formsetName + "-(\\d+|__prefix__))");
+        var name_match = id_regex.exec(input.name);
+        var formsetIndex = name_match && name_match[2];
+        if (formsetIndex) {
+            var replacement = formsetName + "-" + formsetIndex;
+            if (input.hasAttribute("list")) {
+                input.setAttribute("list", input.getAttribute("list").replace(id_regex, replacement));
+            } else if (input.hasAttribute("data-list")) {
+                input.setAttribute("data-list", input.getAttribute("data-list").replace(id_regex, replacement));
+            }
+        }
     }
 
     $(document).ready(function() {
