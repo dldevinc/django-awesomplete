8accc327b84d72eabeeaab08333ea3427959d9ab
pix666 <pix666@ya.ru>
2022-05-30 10:28:40 +0400

    minor fixes in test app

--- tests/app/models.py
+++ tests/app/models.py
@@ -4,10 +4,24 @@ from taggit.managers import TaggableManager
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, blank=True)
-    tags = TaggableManager(blank=True)
-    publish_at = models.DateField(_("publish at"), null=True, blank=True)
+    title = models.CharField(
+        max_length=255
+    )
+    author = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Suggestions are loaded from already populated instances"
+    )
+    tags = TaggableManager(
+        blank=True,
+        help_text="Suggestions are loaded from existing Tag instances"
+    )
+    publish_at = models.DateField(
+        _("publish at"),
+        null=True,
+        blank=True,
+        help_text="Custom suggestions"
+    )
 
     class Meta:
         verbose_name_plural = _("Articles")
